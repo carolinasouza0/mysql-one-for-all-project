ed7ae5ce01af511e2fe94ad9d1227a7ad3c44239
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-08-24 14:39:26 -0300

    feat(req 6): query about faturamento_minimo, faturamento_maximo, faturamento_medio e faturamento_total

---
+++ challenges/desafio6.sql
@@ -0,0 +1,7 @@
+SELECT MIN(p.plan_price) AS faturamento_minimo,
+	MAX(p.plan_price) AS faturamento_maximo,
+    ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
+    SUM(p.plan_price) AS faturamento_total
+FROM SpotifyClone.plans AS p
+INNER JOIN SpotifyClone.users AS us
+ON p.plan_id = us.plan_id;
