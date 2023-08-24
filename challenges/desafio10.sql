67c10c975a2a7cc13c13b8a28f35d0c82708b5f6
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-08-24 19:01:29 -0300

    feat(req 10): createand populate table of favorite songs

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,25 @@
 -- Descomente e altere as linhas abaixo:
+CREATE TABLE SpotifyClone.fav_songs (
+	fav_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    UNIQUE (user_id, song_id),
+    FOREIGN KEY (user_id) REFERENCES SpotifyClone.users(user_id),
+    FOREIGN KEY (song_id) REFERENCES SpotifyClone.songs(song_id)
+) engine = InnoDB;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO SpotifyClone.fav_songs (user_id, song_id)
+    VALUES 
+    (1, 3),
+    (1, 6),
+    (1, 10),
+    (2, 4),
+    (3, 1),
+    (3, 3),
+    (4, 4),
+    (4, 7),
+    (5, 2),
+    (5, 10),
+    (8, 4),
+    (9, 6),
+    (10, 3);
